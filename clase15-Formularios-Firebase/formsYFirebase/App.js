a69e6534a3803680f05ed9f2f38788f7cc4996b6
nelson.salazar@digitalhouse.com <nelson.salazar@digitalhouse.com>
2022-05-25 21:29:36 -0300

    update

--- clase15-Formularios-Firebase/formsYFirebase/App.js
+++
@@ -1,8 +0,0 @@
-import { styleProps } from "react-native-web/dist/cjs/modules/forwardedProps";
-import TabNavigation from "./src/navigation/TabNavigation";
-
-export default function App() {
-  return (
-    <TabNavigation/>
-  );
-}
